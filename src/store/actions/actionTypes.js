668e49693e95127890519ef74c4da58e18954d46
Natalie Gallo <nataliegallo04@gmail.com>
2024-05-04 23:24:20 -0400

    created crud ops for campuses, created new campus container and view

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -6,6 +6,12 @@ export const FETCH_ALL_CAMPUSES = "FETCH_ALL_CAMPUSES";
 //Single campus
 export const FETCH_CAMPUS = "FETCH_CAMPUS";
 
+//Add campus
+export const ADD_CAMPUS = "ADD_CAMPUS";
+
+//Delete campus
+export const DELETE_CAMPUS = "DELETE_CAMPUS";
+
 //All students
 export const FETCH_ALL_STUDENTS = "FETCH_ALL_STUDENTS";
 export const ADD_STUDENT = "ADD_STUDENT";
